bd8bee9adb83692a0777bfbf0ad76c8303fed5a7
Edu <edu8987675@gmail.com>
2025-02-20 13:31:38 +0100

    Prueba de carrusel puesta en componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SongService } from './services/song.service';
 import { MusicaFondoComponent } from './component/musica-fondo/musica-fondo.component';
+import { CarruselFotosComponent } from "./component/carrusel-fotos/carrusel-fotos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, MusicaFondoComponent],
+  imports: [FormsModule, MusicaFondoComponent, CarruselFotosComponent],
   providers: [SongService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
