726574138811eb23c436a4b394c9e9a0fa8606cb
eigucla <eigucla@alumni.uv.es>
2025-02-20 19:24:16 +0100

    cambio a login.js

--- api/login.js
+++ api/login.js
@@ -2,7 +2,7 @@ const querystring = require('querystring');
 
 module.exports = async (req, res) => {
     const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-    const REDIRECT_URI = process.env.REDIRECT_URI || 'https://tu-proyecto.vercel.app/api/callback';
+    const REDIRECT_URI = process.env.REDIRECT_URI || 'https://webboda.vercel.app/api/callback';
     // const REDIRECT_URI = 'http://localhost:3000/callback';
 
     const scope = 'playlist-modify-public playlist-modify-private';
