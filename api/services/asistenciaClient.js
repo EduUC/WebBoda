15334ae8c92931e3a90e29ad4aa2fb136d276041
Edu <edu8987675@gmail.com>
2025-02-22 18:10:58 +0100

    odio esto

--- api/services/asistenciaClient.js
+++ api/services/asistenciaClient.js
@@ -2,15 +2,15 @@ const supabase = require('./supabaseClient');
 require('dotenv').config();
 
 async function agregarAsistencia(nombre, confirmacion) {
-    console.log("✅ Asistencia agregada a la base de datos:", nombre, confirmacion);
-
     const { data, error } = await supabase
         .from('asistencia')
         .insert([{ nombre, confirmacion }]);
 
     if (error) {
         console.error("❌ Error al insertar asistencia:", error);
-        return null;
+        return res.status(500).send("Error al actualizar en la base de datos.");
+    } else {
+        console.log("✅ Asistencia agregada a la base de datos:", nombre, confirmacion);
     }
 
     return data;
