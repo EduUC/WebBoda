d31acd4ec54beb5b48f2cf759d04d13bcfcbd337
Edu <edu8987675@gmail.com>
2025-02-22 18:57:40 +0100

    Cansao 4

--- api/services/asistenciaClient.js
+++ api/services/asistenciaClient.js
@@ -4,15 +4,21 @@ require('dotenv').config();
 async function agregarAsistencia(nombre, confirmacion) {
     console.log("Agregando asistencia:", nombre, confirmacion);
 
-    const { data, error } = await supabase
-        .from('asistencia')
-        .insert([{ nombre, confirmacion }]);
-    if (error) {
-        console.error("❌ Error al insertar asistencia:", error);
-        return res.status(500).send("Error al actualizar en la base de datos.");
-    } else {
+    try {
+        const { data, error } = await supabase
+            .from('asistencia')
+            .insert([{ nombre, confirmacion }]);
+        
+        if (error) {
+            throw new Error(error.message);  // Lanza un error si Supabase devuelve un error
+        }
+        
         console.log("✅ Asistencia agregada a la base de datos:", nombre, confirmacion);
+    } catch (err) {
+        console.error("❌ Error al insertar asistencia:", err);
+        return res.status(500).send("Error al actualizar en la base de datos.");
     }
 }
+
 
 module.exports = { agregarAsistencia };
