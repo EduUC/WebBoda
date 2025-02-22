780cb21128532d27052c45f41706e9ef8ec738f4
Edu <edu8987675@gmail.com>
2025-02-22 17:54:03 +0100

    Asistencia pequeña correcion 6

--- api/backend/add.js
+++ api/backend/add.js
@@ -1,6 +1,10 @@
 // api/spotify/add.js
 const { addAsistencia } = require('../controllers/asistenciaController');
+const multer = require('multer');
+const upload = multer();
 
 module.exports = (req, res) => {
-  addAsistencia(req, res);
+  upload.none()(req, res, () => {
+    addAsistencia(req, res);  
+  };
 };
