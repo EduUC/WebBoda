70b9b4c0e50f2c720ed0dd08e988993310d1fc2e
Edu <edu8987675@gmail.com>
2025-02-22 17:43:12 +0100

    Asistencia pequeña correcion

--- api/controllers/asistenciaController.js
+++ api/controllers/asistenciaController.js
@@ -3,33 +3,33 @@ const { agregarAsistencia } = require('../services/asistenciaClient');
 const addAsistencia = (req, res) => {
     console.log("✅ POST addAsistencia");
 
-    let personas = [];
-    const { num_asistentes } = req.body;
-    const { asistencia } = req.body;
+    // let personas = [];
+    // const { num_asistentes } = req.body;
+    // const { asistencia } = req.body;
 
-    console.log("Body", req.body);
+    // console.log("Body", req.body);
 
 
-    console.log("Asistentes", num_asistentes);
-    console.log("Confirmacion", asistencia);
+    // console.log("Asistentes", num_asistentes);
+    // console.log("Confirmacion", asistencia);
 
-    for (let i = 1; i <= num_asistentes; i++) {
-        const nombre = req.body[`persona-${i}-nombre`];
-        if (nombre) {
-            personas.push({ id: i, nombre });
-        }
-    }
+    // for (let i = 1; i <= num_asistentes; i++) {
+    //     const nombre = req.body[`persona-${i}-nombre`];
+    //     if (nombre) {
+    //         personas.push({ id: i, nombre });
+    //     }
+    // }
 
-    console.log("Personas", personas);
+    // console.log("Personas", personas);
 
     try {
-        for (let i = 1; i <= num_asistentes; i++) {
-            const nombre = req.body[`persona-${i}-nombre`];
-            if (nombre) {
-                personas.push({ id: i, nombre });
-                agregarAsistencia(nombre, asistencia);
-            }
-        }
+    //     for (let i = 1; i <= num_asistentes; i++) {
+    //         const nombre = req.body[`persona-${i}-nombre`];
+    //         if (nombre) {
+    //             personas.push({ id: i, nombre });
+    //             agregarAsistencia(nombre, asistencia);
+    //         }
+    //     }
 
         res.json({
             message: "Asistencia confirmada",
