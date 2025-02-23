8b567a31f4b8a2d09d83e7dcf9ccd173d4ddbd20
Edu <edu8987675@gmail.com>
2025-02-23 18:00:57 +0100

    Quitado 1 serverless porque no se permiten tantos en Vercel

--- api/controllers/asistenciaController.js
+++ api/controllers/asistenciaController.js
@@ -4,24 +4,40 @@ const addAsistencia = async (req, res) => {
     console.log("🔄 POST addAsistencia");
     console.log("Body", req.body);
 
-    const { num_asistentes, asistencia } = req.body;
+    const { num_asistentes, asistencia, tipo_asistencia, dato_adicional } = req.body;
+    const datoAdicional = dato_adicional || " ";
+
+    if (!['ceremonia', 'celebracion'].includes(tipo_asistencia)) {
+        return res.json({
+            message: "Tipo de asistencia no válido",
+            success: false,
+        });
+    }
 
     try {
+        const promises = [];
         for (let i = 1; i <= num_asistentes; i++) {
             const nombre = req.body[`persona-${i}-nombre`];
             if (nombre) {
-                await agregarAsistencia_Ceremonia(nombre, asistencia);
+                // Dependiendo del tipo de asistencia, agrega la correcta
+                if (tipo_asistencia === 'ceremonia') {
+                    promises.push(agregarAsistencia_Ceremonia(nombre, asistencia));
+                } else if (tipo_asistencia === 'celebracion') {
+                    promises.push(agregarAsistencia_Celebracion(nombre, asistencia, datoAdicional));
+                }
             }
         }
 
-        console.log(" ✅Asistencia Ceremonia confirmada");
+        // Espera a que todas las promesas se resuelvan en paralelo
+        await Promise.all(promises);
+
+        console.log(`✅Asistencia ${tipo_asistencia} confirmada`);
 
         res.json({ 
             message: "Asistencia confirmada",
             success: true,
         });
-    }
-    catch (error) {
+    } catch (error) {
         console.error("❌ Error al insertar asistencia:", error);
         res.json({
             message: "Error al confirmar asistencia",
@@ -30,36 +46,4 @@ const addAsistencia = async (req, res) => {
     }
 };
 
-const addAsistencia_Celebracion = async (req, res) => {
-    console.log("🔄 POST addAsistencia_Celebracion");
-    console.log("Body", req.body);
-
-    const { num_asistentes, asistencia, dato_adicional } = req.body;
-    const datoAdicional = dato_adicional || " ";
-
-
-    try {
-        for (let i = 1; i <= num_asistentes; i++) {
-            const nombre = req.body[`persona-${i}-nombre`];
-            if (nombre) {
-                await agregarAsistencia_Celebracion(nombre, asistencia, datoAdicional);
-            }
-        }
-
-        console.log(" ✅Asistencia Celebracion confirmada");
-
-        res.json({ 
-            message: "Asistencia confirmada",
-            success: true,
-        });
-    }
-    catch (error) {
-        console.error("❌ Error al insertar asistencia:", error);
-        res.json({
-            message: "Error al confirmar asistencia",
-            success: false,
-        });
-    }
-}
-
-module.exports = { addAsistencia, addAsistencia_Celebracion };
+module.exports = { addAsistencia };
